a76287217d0a946cbd4f774a0b0df87c842e06c0
CrazyLab1001 <cramerkj@mail.uc.edu>
2025-02-07 11:12:36 -0500

    Task 2 complete!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int birthMonth = 0;
+        String trash = "";
+        System.out.println("What is your birth month (1-12)? ");
+        Scanner in = new Scanner(System.in);
+        if (in.hasNextInt())
+        { birthMonth = in.nextInt(); }
+        else
+        {
+            trash = in.nextLine();
+            System.out.println(trash + " is not a valid input! Try running the program again and entering a number, 1-12. :)");
+            return;
+        }
+        if ((12 >= birthMonth) & (birthMonth >= 1))
+        {
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+        else
+        {
+            System.out.println(birthMonth + " is not a valid number! Try running the program again and entering a number, 1-12. :)");
+            return;
+        }
+
     }
 }
